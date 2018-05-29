b11e55998d7a5f2cd0b5d029dd4a28e62c424b44
Niall Cunningham <hello@niallcunningham.com>
2018-05-29 16:21:08 +0100

    some changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :moorings, only: [:show, :index]
+
+  get "/dashboard", to: "pages#dashboard"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning DB"
 Mooring.destroy_all
-Mooring.create!(name: 'Chocolate canal', longitude: '-62.14805', latitude: '128.51489')
-Mooring.create!(name: 'Stawberry canal', longitude: '25.50254', latitude: '175.39889')
+User.destroy_all
+Booking.destroy_all
+
+puts "Creating moorings"
+mooring_one = Mooring.create!(name: 'Chocolate canal', longitude: '-62.14805', latitude: '128.51489')
+mooring_two = Mooring.create!(name: 'Stawberry canal', longitude: '25.50254', latitude: '175.39889')
+
+puts "Creating one user"
+user = User.create(email: "niall@lewagon.com", password: "123456")
+
+puts "Creating bookings"
+Booking.create(start_date: Date.today, end_date: Date.today + 2, user: user, mooring: mooring_one )
+Booking.create(start_date: Date.today, end_date: Date.today + 2, user: user, mooring: mooring_two )
+
