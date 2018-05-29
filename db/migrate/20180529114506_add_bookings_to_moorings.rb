f4da84c260e2ff790897d3f508377a9ad608bb99
Adrien L <37872504+adrienloesch@users.noreply.github.com>
2018-05-29 12:55:24 +0100

    Update 20180529114506_add_bookings_to_moorings.rb

--- db/migrate/20180529114506_add_bookings_to_moorings.rb
+++ db/migrate/20180529114506_add_bookings_to_moorings.rb
@@ -1,5 +1,5 @@
 class AddBookingsToMoorings < ActiveRecord::Migration[5.2]
   def change
-    add_reference :moorings, :mooring, foreign_key: true
+    add_reference :bookings, :mooring, foreign_key: true
   end
 end
