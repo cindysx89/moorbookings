c4dd8531d3097dc96ffbbd0a25ed2d7f4ec79a88
Cindy Xie <cindyxie89@gmail.com>
2018-05-30 16:25:03 +0100

    figaro gem and geocoder

--- app/controllers/moorings_controller.rb
+++ app/controllers/moorings_controller.rb
@@ -2,6 +2,15 @@ class MooringsController < ApplicationController
 
   def index
     @moorings = Mooring.all
+    @moorings = Mooring.where.not(latitude: nil, longitude: nil)
+
+    @markers = @moorings.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude#,
+        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+      }
+    end
   end
 
   def show
