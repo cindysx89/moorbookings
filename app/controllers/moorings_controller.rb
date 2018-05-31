e4572837c0e9229fca4e4351adb325a3305879e5
Adrien Loesch <adrienloesch@gmail.com>
2018-05-31 10:37:20 +0100

    can see moorings now on GMAPS

--- app/controllers/moorings_controller.rb
+++ app/controllers/moorings_controller.rb
@@ -5,18 +5,13 @@ class MooringsController < ApplicationController
     @moorings = Mooring.all
     @moorings = Mooring.where.not(latitude: nil, longitude: nil)
 
-    @markers = [{
-        lat: 51.506158,
-        lng: -0.087024 }]
-
-
-    # @moorings.map do |mooring|
-    #   {
-    #     lat: mooring.latitude,
-    #     lng: mooring.longitude#,
-    #     # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
-    #   }
-    # end
+    @markers = @moorings.map do |mooring|
+      {
+        lat: mooring.latitude,
+        lng: mooring.longitude#,
+        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+      }
+    end
   end
 
   def show
