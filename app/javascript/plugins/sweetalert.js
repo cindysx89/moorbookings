0409252a8af6b9c57e5dd3bbc84c8b7f3739d447
Niall Cunningham <hello@niallcunningham.com>
2018-06-04 17:41:03 +0100

    alert added

--- app/javascript/plugins/sweetalert.js
+++ app/javascript/plugins/sweetalert.js
@@ -1,26 +1,31 @@
-import swal from 'sweetalert';
+// import swal from 'sweetalert';
 
-function bindSweetAlertButton() {
-  const formSubmitButton = document.getElementById('sweet-alert');
-  if (formSubmitButton) { // protect other pages
-    formSubmitButton.addEventListener('click', (e) => {
-      e.preventDefault();
-      let valueStart = document.getElementById('booking_start_date').value
-      let valueEnd = document.getElementById('booking_end_date').value
-      if ((valueStart === "") || (valueEnd === "")) {
-        alert("Please fill in the dates")
-      } else {
-      const bookingForm = document.getElementById('new_booking')
-      swal({
-        title: "Booking confirmed!",
-        text: "This has been added to your dashboard.",
-        icon: "success"
-      }).then(
-      function(){bookingForm.submit()
-      });
-      }
-    });
-  }
-}
+// function bindSweetAlertButton() {
+//   const formSubmitButton = document.getElementById('sweet-alert');
+//   if (formSubmitButton) { // protect other pages
+//     formSubmitButton.addEventListener('click', (e) => {
+//       e.preventDefault();
 
-export { bindSweetAlertButton };
+//       let valueStart = document.getElementById('booking_start_date').value
+//       let valueEnd = document.getElementById('booking_end_date').value
+//       if ((valueStart === "") || (valueEnd === "")) {
+//         alert("Please fill in the dates")
+//       }
+//       if document.getElementById('new_booking') {
+//         const bookingForm = document.getElementById('new_booking');
+//       } else {
+//         const bookingForm = document.getElementById('edit-form');
+//       }
+//       swal({
+//         title: "Booking confirmed!",
+//         text: "This has been added to your dashboard.",
+//         icon: "success"
+//       }).then(
+//       function(){bookingForm.submit()
+//       });
+//     }
+//   });
+//   }
+// }
+
+  export { bindSweetAlertButton };
